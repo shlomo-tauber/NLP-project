5b912b25251dc17ff67dcfb33b694cc9f88e085b
Omer <BBLN@users.noreply.github.com>
2024-10-19 12:56:59 +0300

    semanticscholar get specter embeddings API

--- semanticscholar_wrapper.py
+++ semanticscholar_wrapper.py
@@ -6,3 +6,7 @@ class SemanticScholarWrapper:
 
     def bulk_search(self, **kwargs):
         return self.client.search_paper(**kwargs, limit=100, bulk=True)
+    
+    def get_specter_embeddings(self, paper_ids):
+        papers = self.client.get_papers(paper_ids, fields=["embedding.specter_v2"])
+        return map(lambda p: p["embedding"]["vector"], papers)
