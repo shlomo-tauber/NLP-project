2543db53811b231b2b711936df6455a4ea534d42
Omer <BBLN@users.noreply.github.com>
2024-10-19 12:58:30 +0300

    specify pinecone index metric

--- pinecone_db.py
+++ pinecone_db.py
@@ -6,12 +6,15 @@ import time
 def get_pinecone_client():
     return Pinecone(api_key=PINECONE_API_KEY)
 
-def get_or_create_index(pc, index_name, dims):
+METRIC_COSINE = "cosine"
+METRIC_L2 = "euclidean"
+
+def get_or_create_index(pc, index_name, dims, metric=METRIC_L2):
     if index_name not in pc.list_indexes().names():
         pc.create_index(
             name=index_name,
-            dimension=dims, # Replace with your model dimensions
-            metric="cosine", # Replace with your model metric
+            dimension=dims, 
+            metric=metric, # Replace with your model metric
             spec=ServerlessSpec(
                 cloud="aws",
                 region="us-east-1"
